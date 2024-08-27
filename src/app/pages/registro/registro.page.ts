44f88a3219b19dab0d9813ca2592744b516aff59
Roberto <robe.roman@duocuc.cl>
2024-08-27 12:56:45 -0400

    Form control al registro

--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { min } from 'rxjs';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  //Aqui creamos variables
+  //Nombre: TIPO = VALOR;
+  //NOMBRE = clase(valor);
+  persona = new FormGroup({
+    //LLAVE: VALOR;
+    rut: new FormControl('',[Validators.minLength(9),Validators.maxLength(10),Validators.required,Validators.pattern("[0-9]{7-8} - [0-9kK]{1}")]),
+    nombre: new FormControl('', [Validators.minLength(3),Validators.required,Validators.pattern("[a-z]{3,5}")]),
+    fecha_nacimiento: new FormControl(),
+    genero: new FormControl()
+
+  });
+
+  constructor(private router: Router){}
 
   ngOnInit() {
   }
 
+  //Aca podemos crear metodos
+  //Ser explicito
+  public registrar():void{
+    //Validaciones? llamar un DAO? conversion?
+    console.log(this.persona.value);
+    alert("Registrao")
+    this.router.navigate(['/login']);
+  }
 }
